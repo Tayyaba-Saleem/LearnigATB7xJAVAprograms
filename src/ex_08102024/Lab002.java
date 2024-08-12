544b50604d8ba79823b36aaa118a70fc1e27ba4e
Tayyaba Saleem <112542713+Tayyaba-Saleem@users.noreply.github.com>
2024-08-12 11:13:13 +0500

    Initial commit 1

--- src/ex_08102024/Lab002.java
+++ src/ex_08102024/Lab002.java
@@ -3,6 +3,6 @@ package ex_08102024;
 public class Lab002 {
     public static void main(String[] args) {
         //Keywords and identifier
-
+        System.out.println("Check git ");
     }
 }
---
+++ src/ex_08102024/Lab03.java
@@ -0,0 +1,7 @@
+package ex_08102024;
+
+public class Lab03 {
+    public static void main(String[] args) {
+
+    }
+}
